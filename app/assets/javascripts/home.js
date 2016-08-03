3c84c03f52653bcbcc60ea2c39d9e3e8f062470c
¨Mario <mario.vizcainos@gmail.com>
2016-08-02 22:19:45 -0500

    create submit method to create a new event

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,4 +3,33 @@ $(document).ready(function() {
     //events: <%= @events.to_json.html_safe %
   });
   $('.modal-trigger').leanModal();
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    format: 'yyyy-mm-dd'
+  });
+});
+// SERIALIZE OBJECTS
+$.fn.serializeObject = function(){
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$( "#create-event-form" ).submit(function( event ) {
+  event.preventDefault();
+  var data = {
+    "event" : $(this).serializeObject()
+  }
+  console.log(data);
 });

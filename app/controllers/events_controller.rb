7c3c6057168ac361345799eba00fbf97cbe9555d
¨Mario <mario.vizcainos@gmail.com>
2016-08-02 22:27:57 -0500

    created events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,16 @@
+class EventsController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def show
+  end
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   get 'home/index'
   root 'home#index'
 end
